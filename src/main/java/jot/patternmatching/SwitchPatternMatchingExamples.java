bc05c1859572b1f43f8b927f3aba90b4777eaa7f
superernie77 <superernie77@gmail.com>
2023-04-17 10:17:41 +0200

    null case added

---
+++ src/main/java/jot/patternmatching/SwitchPatternMatchingExamples.java
@@ -0,0 +1,95 @@
+package jot.patternmatching;
+
+public class SwitchPatternMatchingExamples {
+	
+	// instanceof with pattern matching
+	public static void twitter(Bird bird) {
+		if (bird instanceof Hummingbird humm ) {
+		  System.out.println("Hummingburd twitters: "+humm.twitter() );
+		} else if (bird instanceof Thunderbird thunder) {
+		  System.out.println("Thunderbird twitters: "+thunder.twitter());
+		}
+	  }
+
+	// instanceof before pattern matching
+	public static void twitterOld(Bird bird) {
+		if (bird instanceof Hummingbird) {
+			Hummingbird humm = (Hummingbird)bird;
+			System.out.println("Hummingbird twitters: "+humm.twitter());
+		} else if (bird instanceof Thunderbird ) {
+			Thunderbird thunder = (Thunderbird)bird;
+			System.out.println("Thunderbird twitters: "+thunder.twitter());
+		}
+	}
+	
+	// switch statement with pattern matching
+	public static void switchTwitter(Bird bird) {
+		switch( bird ) {
+			case Hummingbird humm -> System.out.println("Hummingbird twitters: "+ humm.twitter() );
+			case Thunderbird thunder -> 	System.out.println("Thunderbird twitters: "+thunder.twitter());
+		}
+	}
+	
+	// switch  with guarded statements
+	public static void switchTwitterForYoungBirds(Bird bird) {
+		switch( bird ) {
+			case Hummingbird humm when humm.age() < 3 -> System.out.println("Hummingbird twitters: "+ humm.twitter() );
+			case Thunderbird thunder when thunder.age() < 5 -> 	System.out.println("Thunderbird twitters: "+thunder.twitter());
+			default -> System.out.println("Bird is too old to twitter.");
+		}
+	}
+
+	// switch statement with null case
+	public static void switchTwitterWithNullCase(Bird bird) {
+		switch( bird ) {
+			case Hummingbird humm -> System.out.println("Hummingbird twitters: "+ humm.twitter() );
+			case Thunderbird thunder ->	System.out.println("Thunderbird twitters: "+thunder.twitter());
+			case null -> System.out.println("Bird parameter is null.");
+		}
+	}
+	
+}
+
+
+sealed interface Bird permits Thunderbird, Hummingbird {
+	
+	String twitter();
+	
+	int age();
+	
+}
+
+final class Hummingbird implements Bird {
+
+	private int age;
+	
+	public Hummingbird(int age) {
+		this.age = age; 
+	}
+	
+	public int age() {
+		return age;
+	}
+	
+	public String twitter() {
+		return "hummmmm";
+	}
+}
+
+final class Thunderbird implements Bird {
+	
+	private int age;
+	
+	public Thunderbird(int age) {
+		this.age = age; 
+	}
+	
+	public int age() {
+		return age;
+	}
+
+	public String twitter() {
+		return "thunder!";
+	}
+
+}
